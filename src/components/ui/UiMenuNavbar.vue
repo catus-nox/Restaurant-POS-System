a3dc8b0719fb1dde2606d8f480650b87cc11b36b
cetus <cetnox.c19h21h3o.gmail.com>
2024-08-19 04:25:33 +0800

    feat: add menu navbar component

---
+++ src/components/ui/UiMenuNavbar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { watch } from 'vue'
+
+const pages = [
+  { text: '會員中心', pathName: '/' },
+  { text: '點數兌換商城', pathName: '/' },
+  { text: '歷史訂單', pathName: '/' },
+  { text: '店家公告', pathName: '/' },
+  { text: '店家地址', pathName: '/' }
+]
+</script>
+
+<template>
+  <div class="top-18 fixed left-0 z-50 h-full w-[305px] bg-white">
+    <ul>
+      <template v-for="(page, index) in pages" :key="index">
+        <li class="border-tickets-netural-300 border-b text-xl">
+          <RouterLink :to="page.pathName" class="block p-5">{{ page.text }}</RouterLink>
+        </li>
+      </template>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
