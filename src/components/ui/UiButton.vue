3a4ffe6fdf52be43bdc87b4dde94351154daec97
cetus <cetnox.c19h21h3o.gmail.com>
2024-08-18 23:56:02 +0800

    feat: add ui button component and test

---
+++ src/components/ui/UiButton.vue
@@ -0,0 +1,110 @@
+<script setup lang="ts">
+const props = defineProps({
+  btnStyle: String || 'style1' || 'style2' || 'style3' || 'style4',
+  isOnlyIcon: Boolean,
+  leftIcon: Object,
+  rightIcon: Object,
+  fontSize: String,
+  fontPadding: String,
+  iconSize: String,
+  btnWidth: String,
+  btnPadding: String
+})
+</script>
+
+<template>
+  <template v-if="props.btnStyle === 'style1'">
+    <button
+      class="bg-tickets-primary-700 font-Noto-Sans-TC hover:bg-tickets-primary-600 focus:bg-tickets-primary-500 disabled:bg-color-neutral-300 hover:shadow-inner-custom1 focus:shadow-inner-custom2 inline-flex items-center rounded-full disabled:hover:shadow-none"
+      :class="[props.btnWidth, props.btnPadding]"
+    >
+      <component class="stroke-white" :class="props.iconSize" :is="props.leftIcon" />
+      <span
+        class="px-5 font-medium text-white"
+        :class="[props.fontSize, props.fontPadding]"
+        v-if="!props.isOnlyIcon"
+      >
+        <slot></slot>
+      </span>
+      <component class="stroke-white" :class="props.iconSize" :is="props.rightIcon" />
+    </button>
+  </template>
+
+  <template v-if="props.btnStyle === 'style2'">
+    <!-- press2 -->
+    <button
+      class="bg-tickets-primary-50 font-Noto-Sans-TC hover:bg-tickets-primary-100 focus:bg-tickets-primary-100 disabled:bg-color-neutral-300 hover:shadow-inner-custom3 focus:shadow-inner-custom4 shadow-inner-custom3 group inline-flex items-center rounded-full disabled:shadow-none disabled:hover:shadow-none disabled:focus:shadow-none"
+      :class="[props.btnWidth, props.btnPadding]"
+    >
+      <component
+        class="stroke-tickets-primary-700 group-focus:stroke-tickets-primary-900 group-disabled:stroke-white group-[.press2]:stroke-white"
+        :class="props.iconSize"
+        :is="props.leftIcon"
+      />
+      <span
+        class="text-tickets-primary-700 group-focus:text-tickets-primary-900 px-5 font-medium group-disabled:text-white group-[.press2]:text-white"
+        :class="[props.fontSize, props.fontPadding]"
+        v-if="!props.isOnlyIcon"
+      >
+        <slot></slot>
+      </span>
+      <component
+        class="stroke-tickets-primary-700 group-focus:stroke-tickets-primary-900 group-disabled:stroke-white group-[.press2]:stroke-white"
+        :class="props.iconSize"
+        :is="props.rightIcon"
+      />
+    </button>
+  </template>
+
+  <template v-if="props.btnStyle === 'style3'">
+    <button
+      class="bg-tickets-netural-950 shadow-inner-custom5 font-Noto-Sans-TC hover:bg-tickets-primary-700 focus:bg-tickets-primary-600 hover:shadow-inner-custom6 focus:shadow-inner-custom2 inline-flex items-center rounded-full"
+      :class="[props.btnWidth, props.btnPadding]"
+    >
+      <component class="stroke-white" :class="props.iconSize" :is="props.leftIcon" />
+      <span
+        class="px-5 font-medium text-white"
+        :class="[props.fontSize, props.fontPadding]"
+        v-if="!props.isOnlyIcon"
+      >
+        <slot></slot>
+      </span>
+      <component class="stroke-white" :class="props.iconSize" :is="props.rightIcon" />
+    </button>
+  </template>
+
+  <template v-if="props.btnStyle === 'style4'">
+    <!-- press4 -->
+    <button
+      class="font-Noto-Sans-TC hover:bg-tickets-primary-100 focus:bg-tickets-primary-100 disabled:bg-color-neutral-100 hover:shadow-inner-custom7 focus:shadow-inner-custom8 shadow-inner-custom7 disabled:shadow-inner-custom10 group inline-flex items-center rounded-full bg-white"
+      :class="[props.btnWidth, props.btnPadding]"
+    >
+      <component
+        class="stroke-tickets-netural-950 group-hover:stroke-tickets-primary-700 group-focus:stroke-tickets-primary-800 group-[.press4]:stroke-tickets-primary-700 group-disabled:stroke-tickets-netural-400"
+        :class="props.iconSize"
+        :is="props.leftIcon"
+      />
+      <span
+        class="text-tickets-netural-950 group-hover:text-tickets-primary-700 group-focus:text-tickets-primary-800 group-[.press4]:text-tickets-primary-700 group-disabled:text-tickets-netural-400 px-5 font-medium"
+        :class="[props.fontSize, props.fontPadding]"
+        v-if="!props.isOnlyIcon"
+      >
+        <slot></slot>
+      </span>
+      <component
+        class="stroke-tickets-netural-950 group-hover:stroke-tickets-primary-700 group-focus:stroke-tickets-primary-800 group-[.press4]:stroke-tickets-primary-700 group-disabled:stroke-tickets-netural-400"
+        :class="props.iconSize"
+        :is="props.rightIcon"
+      />
+    </button>
+  </template>
+</template>
+
+<style scoped>
+.press2 {
+  @apply bg-tickets-primary-800 shadow-none;
+}
+.press4 {
+  @apply shadow-inner-custom9 bg-white;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,59 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+import testbtn from '@/components/ui/UiButton.vue'
+import testIcon from '@/components/icons/IconCaretLeft.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <testbtn
+      :btn-style="'style1'"
+      :left-icon="testIcon"
+      :btn-width="'w-fit'"
+      :btn-padding="'px-5 py-2'"
+      :icon-size="'w-6 h-6'"
+      :is-only-icon="false"
+      :font-size="'text'"
+      :font-padding="'px-2.5'"
+    >
+      555
+    </testbtn>
+    <testbtn
+      :btn-style="'style2'"
+      :left-icon="testIcon"
+      :btn-width="'w-fit'"
+      :btn-padding="'px-5 py-2'"
+      :icon-size="'w-6 h-6'"
+      :is-only-icon="false"
+      :font-size="'text'"
+      :font-padding="'px-2.5'"
+    >
+      555
+    </testbtn>
+    <testbtn
+      :btn-style="'style3'"
+      :left-icon="testIcon"
+      :btn-width="'w-fit'"
+      :btn-padding="'px-5 py-2'"
+      :icon-size="'w-6 h-6'"
+      :is-only-icon="false"
+      :font-size="'text'"
+      :font-padding="'px-2.5'"
+    >
+      555
+    </testbtn>
+
+    <testbtn
+      :btn-style="'style4'"
+      :left-icon="testIcon"
+      :btn-width="'w-fit'"
+      :btn-padding="'px-5 py-2'"
+      :icon-size="'w-6 h-6'"
+      :is-only-icon="false"
+      :font-size="'text'"
+      :font-padding="'px-2.5'"
+    >
+      555
+    </testbtn>
   </main>
 </template>
