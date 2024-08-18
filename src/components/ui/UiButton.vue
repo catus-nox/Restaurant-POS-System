7229eab4b2e31382788335aa109431e9d1ca7c27
cetus <cetnox.c19h21h3o.gmail.com>
2024-08-19 01:35:21 +0800

    update: adjust ui button style config

--- src/components/ui/UiButton.vue
+++ src/components/ui/UiButton.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { defineProps } from 'vue'
+
 const props = defineProps({
   btnStyle: String || 'style1' || 'style2' || 'style3' || 'style4',
   isOnlyIcon: Boolean,
@@ -15,12 +17,12 @@ const props = defineProps({
 <template>
   <template v-if="props.btnStyle === 'style1'">
     <button
-      class="bg-tickets-primary-700 font-Noto-Sans-TC hover:bg-tickets-primary-600 focus:bg-tickets-primary-500 disabled:bg-color-neutral-300 hover:shadow-inner-custom1 focus:shadow-inner-custom2 inline-flex items-center rounded-full disabled:hover:shadow-none"
+      class="bg-tickets-primary-700 font-Noto-Sans-TC hover:bg-tickets-primary-600 focus:bg-tickets-primary-500 disabled:bg-color-neutral-300 hover:shadow-inner-custom1 focus:shadow-inner-custom2 flex items-center justify-center rounded-full disabled:hover:shadow-none"
       :class="[props.btnWidth, props.btnPadding]"
     >
       <component class="stroke-white" :class="props.iconSize" :is="props.leftIcon" />
       <span
-        class="px-5 font-medium text-white"
+        class="px-5 font-normal text-white"
         :class="[props.fontSize, props.fontPadding]"
         v-if="!props.isOnlyIcon"
       >
@@ -33,7 +35,7 @@ const props = defineProps({
   <template v-if="props.btnStyle === 'style2'">
     <!-- press2 -->
     <button
-      class="bg-tickets-primary-50 font-Noto-Sans-TC hover:bg-tickets-primary-100 focus:bg-tickets-primary-100 disabled:bg-color-neutral-300 hover:shadow-inner-custom3 focus:shadow-inner-custom4 shadow-inner-custom3 group inline-flex items-center rounded-full disabled:shadow-none disabled:hover:shadow-none disabled:focus:shadow-none"
+      class="bg-tickets-primary-50 font-Noto-Sans-TC hover:bg-tickets-primary-100 focus:bg-tickets-primary-100 disabled:bg-color-neutral-300 hover:shadow-inner-custom3 focus:shadow-inner-custom4 shadow-inner-custom3 group flex items-center justify-center rounded-full disabled:shadow-none disabled:hover:shadow-none disabled:focus:shadow-none"
       :class="[props.btnWidth, props.btnPadding]"
     >
       <component
@@ -42,7 +44,7 @@ const props = defineProps({
         :is="props.leftIcon"
       />
       <span
-        class="text-tickets-primary-700 group-focus:text-tickets-primary-900 px-5 font-medium group-disabled:text-white group-[.press2]:text-white"
+        class="text-tickets-primary-700 group-focus:text-tickets-primary-900 px-5 font-normal group-disabled:text-white group-[.press2]:text-white"
         :class="[props.fontSize, props.fontPadding]"
         v-if="!props.isOnlyIcon"
       >
@@ -58,12 +60,12 @@ const props = defineProps({
 
   <template v-if="props.btnStyle === 'style3'">
     <button
-      class="bg-tickets-netural-950 shadow-inner-custom5 font-Noto-Sans-TC hover:bg-tickets-primary-700 focus:bg-tickets-primary-600 hover:shadow-inner-custom6 focus:shadow-inner-custom2 inline-flex items-center rounded-full"
+      class="bg-tickets-netural-950 shadow-inner-custom5 font-Noto-Sans-TC hover:bg-tickets-primary-700 focus:bg-tickets-primary-600 hover:shadow-inner-custom6 focus:shadow-inner-custom2 flex items-center justify-center rounded-full"
       :class="[props.btnWidth, props.btnPadding]"
     >
       <component class="stroke-white" :class="props.iconSize" :is="props.leftIcon" />
       <span
-        class="px-5 font-medium text-white"
+        class="px-5 font-normal text-white"
         :class="[props.fontSize, props.fontPadding]"
         v-if="!props.isOnlyIcon"
       >
@@ -76,7 +78,7 @@ const props = defineProps({
   <template v-if="props.btnStyle === 'style4'">
     <!-- press4 -->
     <button
-      class="font-Noto-Sans-TC hover:bg-tickets-primary-100 focus:bg-tickets-primary-100 disabled:bg-color-neutral-100 hover:shadow-inner-custom7 focus:shadow-inner-custom8 shadow-inner-custom7 disabled:shadow-inner-custom10 group inline-flex items-center rounded-full bg-white"
+      class="font-Noto-Sans-TC hover:bg-tickets-primary-100 focus:bg-tickets-primary-100 disabled:bg-color-neutral-100 hover:shadow-inner-custom7 focus:shadow-inner-custom8 shadow-inner-custom7 disabled:shadow-inner-custom10 group flex items-center justify-center rounded-full bg-white"
       :class="[props.btnWidth, props.btnPadding]"
     >
       <component
@@ -85,7 +87,7 @@ const props = defineProps({
         :is="props.leftIcon"
       />
       <span
-        class="text-tickets-netural-950 group-hover:text-tickets-primary-700 group-focus:text-tickets-primary-800 group-[.press4]:text-tickets-primary-700 group-disabled:text-tickets-netural-400 px-5 font-medium"
+        class="text-tickets-netural-950 group-hover:text-tickets-primary-700 group-focus:text-tickets-primary-800 group-[.press4]:text-tickets-primary-700 group-disabled:text-tickets-netural-400 px-5 font-normal"
         :class="[props.fontSize, props.fontPadding]"
         v-if="!props.isOnlyIcon"
       >
