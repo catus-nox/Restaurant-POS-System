813eef68b53417b2ef34c8d96eeb65f3346e6969
cetus <cetnox.c19h21h3o.gmail.com>
2024-08-19 04:40:59 +0800

    update: add menu bar component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import UiMenubar from '@/components/ui/UiMenubar.vue'
 </script>
 
 <template>
   <body class="bg-tickets-primary-50 m-auto min-h-screen w-full max-w-screen-tablet">
-    <!-- <header></header> -->
+    <UiMenubar />
     <main>
       <RouterView />
     </main>
-    <!-- <footer></footer> -->
   </body>
 </template>
 
