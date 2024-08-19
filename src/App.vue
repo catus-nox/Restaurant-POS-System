25a5f884321e6f9a9ce163fefea18cd9f9361485
cetus <cetnox.c19h21h3o.gmail.com>
2024-08-19 16:56:47 +0800

    update: modify layout code and import  component structure

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@ import UiMenubar from '@/components/ui/UiMenubar.vue'
 </script>
 
 <template>
-  <body class="bg-tickets-primary-50 m-auto min-h-screen w-full max-w-screen-tablet">
+  <body
+    class="bg-tickets-primary-50 font-Noto-Sans-TC m-auto min-h-screen w-full max-w-screen-tablet"
+  >
     <UiMenubar />
     <main>
       <RouterView />
--- src/views/CustomerMenuView.vue
+++ src/views/CustomerMenuView.vue
@@ -1,6 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UiShopInformation from '@/components/ui/UiShopInformation.vue'
+import UiCustomerMenuNavbar from '@/components/ui/UiCustomerMenuNavbar.vue'
+import UiProductItem from '@/components/ui/UiProductItem.vue'
+import IconSearch from '@/components/icons/IconSearch.vue'
+</script>
 
 <template>
-  <div class="">菜單</div>
+  <div class="m-3 flex flex-col gap-3">
+    <UiShopInformation />
+
+    <!-- input -->
+    <div class="border-tickets-netural-900 w-full rounded-full border bg-white px-3 py-2 pl-4">
+      <!-- <label for="Search">Name</label> -->
+      <div class="flex flex-row gap-2">
+        <IconSearch class="fill-tickets-netural-900 h-6 w-6" />
+        <input
+          type="text"
+          id="Search"
+          placeholder="Search"
+          class="text-gray-500 placeholder:text-gray-500 focus-visible:outline-none"
+        />
+      </div>
+    </div>
+
+    <UiCustomerMenuNavbar />
+
+    <h2 class="text-h5 font-bold text-black">義式咖啡</h2>
+
+    <div v-for="index in 10" :key="index">
+      <UiProductItem />
+    </div>
+  </div>
 </template>
 <style scoped></style>
