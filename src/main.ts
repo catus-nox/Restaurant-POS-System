add799bb0ec6ae528de603b125fff3e1c742c6c2
cetus <cetnox.c19h21h3o.gmail.com>
2024-08-29 16:17:37 +0800

    update: add router change scroll position

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//切換頁面後顯示最上方
+router.afterEach((to, from, failure) => {
+  window.scrollTo(0, 0)
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
