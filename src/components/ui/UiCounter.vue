281b848f76913f2b7fc347ba10ea81fe2f709d17
cetus <cetnox.c19h21h3o.gmail.com>
2024-09-05 15:56:59 +0800

    hotfix: add import defineProps

--- src/components/ui/UiCounter.vue
+++ src/components/ui/UiCounter.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { defineProps } from 'vue'
 import UiButton from '@/components/ui/UiButton.vue'
 
 const counterProps = defineProps({
--- src/components/ui/UiCustomerMenuNavbar.vue
+++ src/components/ui/UiCustomerMenuNavbar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { defineProps } from 'vue'
 
 interface MenuCategoryItem {
   categoryId?: any // Id
