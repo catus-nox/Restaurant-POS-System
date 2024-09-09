e0826656068efce646c902cbd594e5649aab8df6
cetus <cetnox.c19h21h3o.gmail.com>
2024-09-10 01:00:41 +0800

    feature: add workspace state option component

--- src/components/views/employee/foh/orderView.vue
+++ src/components/views/employee/foh/orderView.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import EmployeeUiSearchAndFilterBar from '@/components/ui/employee/UiSearchAndFilterBar.vue'
 import EmployeeUiWorkspaceStateNavbar from '@/components/ui/employee/UiWorkspaceStateNavbar.vue'
+import EmployeeUiWorkspaceStateOption from '@/components/ui/employee/UiWorkspaceStateOption.vue'
 </script>
 
 <template>
   <EmployeeUiSearchAndFilterBar />
   <EmployeeUiWorkspaceStateNavbar />
+
+  <div class="grid grid-cols-3 gap-6 p-6">
+    <template v-for="index in 9" :key="index">
+      <EmployeeUiWorkspaceStateOption />
+    </template>
+  </div>
 </template>
 <style scoped></style>
