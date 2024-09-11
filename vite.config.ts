dad016b04e79c8cbe71c1007679016ca0a811ff8
cetus <cetnox.c19h21h3o.gmail.com>
2024-09-11 14:28:05 +0800

    hotfix: add vercel env setting

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,25 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
+import dotenv from 'dotenv'
+dotenv.config()
+const baseURL = process.env.VITE_APP_API_URL
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      // [`/${apiName}`]: {
+      '/api': {
+        target: baseURL,
+        changeOrigin: true
+        // 如果有字串需要替換可以加入 rewrite 這個屬性
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
 })
