64d8893b9f5b2395fdc694515937ee21bb3b6e23
cetus <cetnox.c19h21h3o.gmail.com>
2024-09-17 23:40:21 +0800

    feat: add foh get order count api to order page

--- src/components/views/employee/foh/orderView.vue
+++ src/components/views/employee/foh/orderView.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
+import { ref, computed, onMounted } from 'vue'
+import { useEmployeeStore } from '@/stores/employee/productsStore'
 import EmployeeUiSearchAndFilterBar from '@/components/ui/employee/UiSearchAndFilterBar.vue'
 import EmployeeUiWorkspaceStateNavbar from '@/components/ui/employee/UiWorkspaceStateNavbar.vue'
 import EmployeeUiWorkspaceStateOption from '@/components/ui/employee/UiWorkspaceStateOption.vue'
 import EmployeeUiOrderDetailsNavbar from '@/components/ui/employee/UiOrderDetailsNavbar.vue'
+//-----
+//api
+const employeeStore = useEmployeeStore()
+//取得今日訂單數量與頁數
+const fohGetOrderCountData: any = computed(() => employeeStore.getFohGetOrderCountData)
+
+//-----
+onMounted(async () => {
+  //取得今日訂單數量與頁數
+  await employeeStore.fetchEmployeeFohGetOrderCount(localStorage.token)
+
+  console.log(fohGetOrderCountData.value)
+})
 </script>
 
 <template>
