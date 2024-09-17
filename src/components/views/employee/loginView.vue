ad79bde71b0b652ff02ba5d7bf9d1d20620cdf5b
cetus <cetnox.c19h21h3o.gmail.com>
2024-09-17 15:28:20 +0800

    feat: add account and password validation

--- src/components/views/employee/loginView.vue
+++ src/components/views/employee/loginView.vue
@@ -1,6 +1,28 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import UiButton from '@/components/ui/UiButton.vue'
 import UiInput from '@/components/ui/UiInput.vue'
+import {
+  accountValidateData,
+  validateAccount,
+  passwordValidateData,
+  validatePassword
+} from '@/models/validate'
+
+//-----
+//帳號
+const account = ref('')
+//帳號驗證結果
+const isValidAccount = ref<boolean>(false)
+//帳號是否點擊過輸入框
+const isTouchAccount = ref<boolean>(false)
+//-----
+//密碼
+const password = ref('')
+//密碼驗證結果
+const isValidPassword = ref<boolean>(false)
+//密碼是否點擊過輸入框
+const isTouchPassword = ref<boolean>(false)
 </script>
 
 <template>
@@ -8,7 +30,7 @@ import UiInput from '@/components/ui/UiInput.vue'
     <div class="flex w-full max-w-[459px] flex-col items-center gap-4">
       <img src="../../../assets/img/logo/character-black.png" class="max-w-72" alt="" />
       <div
-        class="bg-neutral-0 flex w-full max-w-[459px] flex-col items-center gap-4 gap-7 rounded-3xl border border-neutral-950 p-5"
+        class="flex w-full max-w-[459px] flex-col items-center gap-4 gap-7 rounded-3xl border border-neutral-950 bg-neutral-0 p-5"
       >
         <div class="flex w-full flex-col gap-4">
           <UiInput
@@ -17,9 +39,15 @@ import UiInput from '@/components/ui/UiInput.vue'
             :placeholder="'請輸入手機號碼'"
             :is-important="true"
             :type="'text'"
+            v-model="account"
+            @define-focus-function="isTouchAccount = true"
+            @define-input-function="validateAccount(isValidAccount, account)"
+            :is-validation-message="validateAccount(isValidAccount, account) !== isTouchAccount"
           >
-            <template #helper></template>
-            <template #validationMessage></template>
+            <template #helper>{{ accountValidateData.helper }}</template>
+            <template #validationMessage>
+              {{ accountValidateData.validationMessage }}
+            </template>
           </UiInput>
           <UiInput
             :is-label="true"
@@ -27,9 +55,15 @@ import UiInput from '@/components/ui/UiInput.vue'
             :placeholder="'請輸入密碼'"
             :is-important="true"
             :type="'text'"
+            v-model="password"
+            @define-focus-function="isTouchPassword = true"
+            @define-input-function="validatePassword(isValidPassword, password)"
+            :is-validation-message="validatePassword(isValidPassword, password) !== isTouchPassword"
           >
-            <template #helper></template>
-            <template #validationMessage></template>
+            <template #helper>{{ passwordValidateData.helper }}</template>
+            <template #validationMessage>
+              {{ passwordValidateData.validationMessage }}
+            </template>
           </UiInput>
         </div>
         <div class="flex w-full flex-col items-center gap-2">
