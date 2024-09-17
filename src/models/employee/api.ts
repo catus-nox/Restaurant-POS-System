afee3fc4d7639dbb96a77e5d25af13c827c556d1
cetus <cetnox.c19h21h3o.gmail.com>
2024-09-17 23:38:28 +0800

    feat: add foh get order count api to pinia

--- src/models/employee/api.ts
+++ src/models/employee/api.ts
@@ -4,10 +4,37 @@ import request from '@/plugins/axios'
 export function postEmployeeLogin(data: { account: string; password: string }) {
   return request({
     url: `/employee/login`,
-    method: 'POST',
+    method: 'post',
     data: {
       account: data.account,
       password: data.password
     }
+  })
+}
+
+//取得今日訂單數量與頁數
+export function getEmployeeFohGetOrderCount(
+  token: string, // 添加 token 參數
+  //orderStatus為空的話就會回傳全部訂單的結果 (全部訂單不會抓"已完成"的)
+  orderStatus?:
+    | '全部訂單'
+    | '0'
+    | '待結帳'
+    | '2'
+    | '準備中'
+    | '3'
+    | '待取餐'
+    | '4'
+    | '已完成'
+    | '5',
+  data?: any
+) {
+  return request({
+    url: `/foh/getOrderCount/${orderStatus}`,
+    method: 'get',
+    headers: {
+      Authorization: `Bearer ${token}` // 在 headers 中添加 Authorization
+    },
+    data //params:
   })
 }
--- src/stores/employee/productsStore.ts
+++ src/stores/employee/productsStore.ts
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
-import { postEmployeeLogin } from '@/models/employee/api'
+import { postEmployeeLogin, getEmployeeFohGetOrderCount } from '@/models/employee/api'
 
 export const useEmployeeStore = defineStore('employee', () => {
   //------
@@ -8,12 +8,16 @@ export const useEmployeeStore = defineStore('employee', () => {
 
   //員工登入
   const loginData = ref()
+  //取得今日訂單數量與頁數
+  const fohGetOrderCountData = ref()
 
   //------
   //getter
 
   //員工登入
   const getLoginData = computed(() => loginData.value)
+  //取得今日訂單數量與頁數
+  const getFohGetOrderCountData = computed(() => fohGetOrderCountData.value)
 
   //------
   //action 異步請求
@@ -37,8 +41,35 @@ export const useEmployeeStore = defineStore('employee', () => {
     }
   }
 
+  //取得今日訂單數量與頁數
+  const fetchEmployeeFohGetOrderCount = async (
+    token: string, // 添加 token 參數
+    orderStatus?:
+      | '全部訂單'
+      | '0'
+      | '待結帳'
+      | '2'
+      | '準備中'
+      | '3'
+      | '待取餐'
+      | '4'
+      | '已完成'
+      | '5'
+  ) => {
+    // 如果 orderStatus 是 undefined，則給它一個默認值，例如 '全部訂單'
+    const status = orderStatus || '全部訂單'
+    try {
+      const response = await getEmployeeFohGetOrderCount(token, status)
+      fohGetOrderCountData.value = response.data.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return {
     getLoginData,
-    fetchEmployeeLogin
+    fetchEmployeeLogin,
+    getFohGetOrderCountData,
+    fetchEmployeeFohGetOrderCount
   }
 })
