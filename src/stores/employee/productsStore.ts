0932cf205921936ec0ffb96bdf8f1f5ce5c40bf0
cetus <cetnox.c19h21h3o.gmail.com>
2024-09-18 01:20:40 +0800

    feat: add foh get order api to pinia

--- src/stores/employee/productsStore.ts
+++ src/stores/employee/productsStore.ts
@@ -1,6 +1,10 @@
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
-import { postEmployeeLogin, getEmployeeFohGetOrderCount } from '@/models/employee/api'
+import {
+  postEmployeeLogin,
+  getEmployeeFohGetOrderCount,
+  getEmployeeFohGetOrder
+} from '@/models/employee/api'
 
 export const useEmployeeStore = defineStore('employee', () => {
   //------
@@ -10,6 +14,8 @@ export const useEmployeeStore = defineStore('employee', () => {
   const loginData = ref()
   //取得今日訂單數量與頁數
   const fohGetOrderCountData = ref()
+  //外場訂單總覽
+  const fohGetOrderData = ref()
 
   //------
   //getter
@@ -18,6 +24,8 @@ export const useEmployeeStore = defineStore('employee', () => {
   const getLoginData = computed(() => loginData.value)
   //取得今日訂單數量與頁數
   const getFohGetOrderCountData = computed(() => fohGetOrderCountData.value)
+  //外場訂單總覽
+  const getFohGetOrderData = computed(() => fohGetOrderData.value)
 
   //------
   //action 異步請求
@@ -54,9 +62,8 @@ export const useEmployeeStore = defineStore('employee', () => {
       | '待取餐'
       | '4'
       | '已完成'
-      | '5'
+      | '5' //如果 orderStatus 是 undefined，則給它一個默認值，例如 '全部訂單' //(全部訂單不會抓"已完成"的)
   ) => {
-    // 如果 orderStatus 是 undefined，則給它一個默認值，例如 '全部訂單'
     const status = orderStatus || '全部訂單'
     try {
       const response = await getEmployeeFohGetOrderCount(token, status)
@@ -66,10 +73,39 @@ export const useEmployeeStore = defineStore('employee', () => {
     }
   }
 
+  //外場訂單總覽
+  const fetchEmployeeFohGetOrder = async (getData: {
+    token: string // 添加 token 參數
+    page?: number // 抓該頁的1~9筆訂單  (為空或其他值會傳第一頁)
+    orderStatus?:
+      | '全部訂單'
+      | '0'
+      | '待結帳'
+      | '2'
+      | '準備中'
+      | '3'
+      | '待取餐'
+      | '4'
+      | '已完成'
+      | '5' //(全部訂單不會抓"已完成"的)
+    type?: '0' | '全部訂單' | '內用' | '1' | '外帶' | '2' | '預約自取' | '3' //用餐類型，為空的話也是傳所有類型的(外帶跟預約自取都算外帶)
+    orderBy?: '時間越早優先' | '時間越晚優先' //依據排序，為空的話會以"時間越早優先"為主
+    search?: any //依據值來搜尋
+  }) => {
+    try {
+      const response = await getEmployeeFohGetOrder(getData)
+      fohGetOrderData.value = response.data.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return {
     getLoginData,
     fetchEmployeeLogin,
     getFohGetOrderCountData,
-    fetchEmployeeFohGetOrderCount
+    fetchEmployeeFohGetOrderCount,
+    getFohGetOrderData,
+    fetchEmployeeFohGetOrder
   }
 })
