6f19cd1337bfaba46945ce6c59b9fd390d7738a7
cetus <cetnox.c19h21h3o.gmail.com>
2024-09-18 22:08:15 +0800

    update: add api function, then get foh get order count api and get foh get order api

--- src/components/views/employee/foh/orderView.vue
+++ src/components/views/employee/foh/orderView.vue
@@ -5,93 +5,55 @@ import EmployeeUiSearchAndFilterBar from '@/components/ui/employee/UiSearchAndFi
 import EmployeeUiWorkspaceStateNavbar from '@/components/ui/employee/UiWorkspaceStateNavbar.vue'
 import EmployeeUiWorkspaceStateOption from '@/components/ui/employee/UiWorkspaceStateOption.vue'
 import EmployeeUiOrderDetailsNavbar from '@/components/ui/employee/UiOrderDetailsNavbar.vue'
+
 //-----
 //api
 const employeeStore = useEmployeeStore()
-//取得今日訂單數量與頁數
-const fohGetOrderCount: any = computed(() => employeeStore.getFohGetOrderCountData)
+//-----
+//取得今日全部訂單數量與頁數
+const fohGetOrderAllCountData = computed(() => employeeStore.getFohGetOrderAllCountData)
 //外場訂單總覽
 const fohGetOrder: any = computed(() => employeeStore.getFohGetOrderData)
 //-----
 //訂單狀態控制
-const nowClick: any = ref(0)
+const nowOrderStatusClick: any = ref(0)
 async function toggleMenu(index: number) {
-  if (index >= 0) {
-    nowClick.value = index
-  } else {
-    nowClick.value = index + 1
-  }
+  nowOrderStatusClick.value = index
 
-  let getData: {
-    token: string // 添加 token 參數
-    page?: number // 抓該頁的1~9筆訂單  (為空或其他值會傳第一頁)
-    orderStatus?:
-      | '全部訂單'
-      | '0'
-      | '待結帳'
-      | '2'
-      | '準備中'
-      | '3'
-      | '待取餐'
-      | '4'
-      | '已完成'
-      | '5' //(全部訂單不會抓"已完成"的)
-    type?: '0' | '全部訂單' | '內用' | '1' | '外帶' | '2' | '預約自取' | '3' //用餐類型，為空的話也是傳所有類型的(外帶跟預約自取都算外帶)
-    orderBy?: '時間越早優先' | '時間越晚優先' //依據排序，為空的話會以"時間越早優先"為主
-    search?: any //依據值來搜尋
-  } = {
-    token: localStorage.token,
-    page: fohGetOrderCount.value.pageCount,
-    orderStatus: '待結帳',
-    type: '全部訂單',
-    orderBy: '時間越早優先'
+  let nowOrderStatus = 0
+  if (!(index == 0)) {
+    nowOrderStatus = index + 1
   }
+  //取得今日訂單數量與頁數
+  await employeeStore.fetchEmployeeFohGetAllOrderCount()
+
   //外場訂單總覽
-  await employeeStore.fetchEmployeeFohGetOrder(getData)
+  let orderGetData: any = {
+    orderStatus: nowOrderStatus
+  }
+  await employeeStore.fetchEmployeeFohGetOrder(orderGetData)
 }
-
 //-----
 onMounted(async () => {
-  //取得今日訂單數量與頁數
-  await employeeStore.fetchEmployeeFohGetOrderCount(localStorage.token)
-
-  let getData: {
-    token: string // 添加 token 參數
-    page?: number // 抓該頁的1~9筆訂單  (為空或其他值會傳第一頁)
-    orderStatus?:
-      | '全部訂單'
-      | '0'
-      | '待結帳'
-      | '2'
-      | '準備中'
-      | '3'
-      | '待取餐'
-      | '4'
-      | '已完成'
-      | '5' //(全部訂單不會抓"已完成"的)
-    type?: '0' | '全部訂單' | '內用' | '1' | '外帶' | '2' | '預約自取' | '3' //用餐類型，為空的話也是傳所有類型的(外帶跟預約自取都算外帶)
-    orderBy?: '時間越早優先' | '時間越晚優先' //依據排序，為空的話會以"時間越早優先"為主
-    search?: any //依據值來搜尋
-  } = {
-    token: localStorage.token,
-    page: fohGetOrderCount.value.pageCount,
-    orderStatus: '全部訂單',
-    type: '全部訂單',
-    orderBy: '時間越早優先'
-  }
+  //取得今日全部訂單數量與頁數
+  await employeeStore.fetchEmployeeFohGetAllOrderCount()
   //外場訂單總覽
-  await employeeStore.fetchEmployeeFohGetOrder(getData)
+  await employeeStore.fetchEmployeeFohGetOrder()
 })
 </script>
 
 <template>
   <div class="h-full grow">
     <EmployeeUiSearchAndFilterBar />
-    <EmployeeUiWorkspaceStateNavbar :index="nowClick" @toggle-menu="toggleMenu" />
-
+    <EmployeeUiWorkspaceStateNavbar
+      v-if="fohGetOrderAllCountData"
+      :now-order-status-click="nowOrderStatusClick"
+      :order-all-count-data="fohGetOrderAllCountData"
+      @toggle-menu="toggleMenu"
+    />
     <div class="grid grid-cols-3 gap-6 p-6">
-      <template v-for="index in fohGetOrder" :key="index">
-        <EmployeeUiWorkspaceStateOption />
+      <template v-for="(order, index) in fohGetOrder" :key="index">
+        <EmployeeUiWorkspaceStateOption :order-data="order" />
       </template>
     </div>
   </div>
