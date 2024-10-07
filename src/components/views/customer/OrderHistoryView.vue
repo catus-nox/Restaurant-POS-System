602d8c4576892b3b2576f7d01aa5d10ec9ebf0ca
cetus <cetnox.c19h21h3o.gmail.com>
2024-10-08 02:16:21 +0800

    hotfix: remove rearrangement button in completedOrders

--- src/components/views/customer/OrderHistoryView.vue
+++ src/components/views/customer/OrderHistoryView.vue
@@ -395,35 +395,6 @@ onMounted(async () => {
               <div class="flex justify-between gap-2">
                 <div>訂單進度 :</div>
                 <div class="flex items-center gap-2">
-                  <UiButton
-                    :btn-style="'style4'"
-                    :btn-width="'size-6'"
-                    :btn-padding="''"
-                    :icon-size="''"
-                    :is-only-icon="true"
-                    :font-size="'text-xs font-medium'"
-                    @define-function="rearrangementFunction"
-                  >
-                    <template #only-icon>
-                      <svg
-                        class="size-4 text-gray-800"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                      >
-                        <path
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"
-                        />
-                      </svg>
-                    </template>
-                  </UiButton>
                   {{ order.orderStatus }}
                 </div>
               </div>
